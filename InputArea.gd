a594742455f909c52dc26c28b2ad3975b8f04c4b
Will Whitty <tavurth@gmail.com>
2021-01-22 00:26:49 +0300

    Simplify

--- InputArea.gd
+++ InputArea.gd
@@ -13,4 +13,8 @@ func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		var pos = event.position
 
+		# Since we're using a control the Y-axis is inverted
+		# Here we'll flip it back to how it should be
+		pos.y = get_viewport_rect().size.y - pos.y
+
 		self.material.set_shader_param("mouse_position", pos)
