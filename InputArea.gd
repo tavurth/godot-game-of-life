93b2e1d92199e936c742309e84671c9a90bc2235
Will Whitty <tavurth@gmail.com>
2021-01-22 15:58:40 +0300

    Updates for web export

--- InputArea.gd
+++ InputArea.gd
@@ -19,10 +19,4 @@ func _input(event: InputEvent):
 	if event is InputEventMouseMotion:
 		var pos = event.position
 
-		# Flip mouse Y-Axis
-		pos.y = get_viewport_rect().size.y - pos.y
-
-		# We're using a double viewport, correct the mouse offset caused by this
-		pos = pos / 2 + get_viewport_rect().size / 2;
-
 		Renderer.material.set_shader_param("mouse_position", pos)
